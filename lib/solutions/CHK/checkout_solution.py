0a0e1acd967c0748507c55f562fa18981d0a73aa
sbx_user1060 <sbx_user1060@ip-10-128-242-216.eu-west-2.compute.internal>
2019-03-24 16:04:42 +0000

    Sun Mar 24 16:04:42 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,19 +1,16 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-	# assumes input skus is in format ABDACB i.e. no delimeter
 
 	# data
 	prices = {'A':50, 'B':30, 'C':20, 'D':15}
 	deals = [] # list of deals, 1 deal is a list in format [SKU, number required, price]
-	deals.append(['A', 3, 130])
-	deals.append(['B', 2, 45])
+	deals.append(['A', 3, 130, 0])
+	deals.append(['B', 2, 45, 0])
 
 	# main loop
 	total_cost = 0
-	for sku in skus: # SKU is hopefully 1 character
-		if sku in prices:
+	for sku in skus: # SKU is 1 character
+		if sku in prices: # hopefully will avoid delimiter which I do not know
 
 	return total_cost
