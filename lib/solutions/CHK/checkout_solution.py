665ef8402ed11c3bf00e83b2af1b25e668704539
sbx_user1060 <sbx_user1060@ip-10-128-242-216.eu-west-2.compute.internal>
2019-03-24 16:33:22 +0000

    Sun Mar 24 16:33:22 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,5 @@
 # data
-PRICES = {'A':50, 'B':30, 'C':20, 'D':15} # menu
+PRICES = {'A':50, 'B':30, 'C':20, 'D':15, 'E':40} # menu
 DEALS = {} # dictionary of deals, 1 deal is a list in format {SKU: [number required, price]}
 DEALS['A'] = [3, 130]
 DEALS['B'] = [2, 45]
@@ -26,3 +26,4 @@ def checkout(skus):
 			total_cost += n*PRICES[sku] # no deals
 
 	return total_cost
+
