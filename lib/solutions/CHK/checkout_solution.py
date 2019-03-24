996887856b8732da850a5e35bd3520d438510e79
sbx_user1060 <sbx_user1060@ip-10-128-242-216.eu-west-2.compute.internal>
2019-03-24 16:01:42 +0000

    Sun Mar 24 16:01:42 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,4 +10,10 @@ def checkout(skus):
 	deals = [] # list of deals, 1 deal is a list in format [SKU, number required, price]
 	deals.append(['A', 3, 130])
 	deals.append(['B', 2, 45])
-	for sku in skus:
+
+	# main loop
+	total_cost = 0
+	for sku in skus: # SKU is hopefully 1 character
+		if sku in prices:
+
+	return total_cost
