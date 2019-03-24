8a8310532f20886c625ba131e67fcf5aa2d0940c
sbx_user1060 <sbx_user1060@ip-10-128-242-216.eu-west-2.compute.internal>
2019-03-24 15:58:42 +0000

    Sun Mar 24 15:58:42 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,10 @@
 # skus = unicode string
 def checkout(skus):
 	# assumes input skus is in format ABDACB i.e. no delimeter
-    prices = {'A':50, 'B':30, 'C':20, 'D':15}
-    deals = []
 
-
+	# data
+	prices = {'A':50, 'B':30, 'C':20, 'D':15}
+	deals = [] # list of deals, 1 deal is a list in format [SKU, number required, price]
+	deals.append(['A', 3, 130])
+	deals.append(['B', 2, 45])
+	for sku in skus:
