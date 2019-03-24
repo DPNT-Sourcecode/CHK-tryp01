7a66ca5ff00c45d06ff29fb2097a5ef7590fecff
sbx_user1060 <sbx_user1060@ip-10-128-242-216.eu-west-2.compute.internal>
2019-03-24 16:13:42 +0000

    Sun Mar 24 16:13:42 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,7 @@
 # data
 PRICES = {'A':50, 'B':30, 'C':20, 'D':15}
-DEALS = [] # list of deals, 1 deal is a list in format [SKU, number required, price, number have]
-DEALS.append(['A', 3, 130])
+DEALS = {} # dictionary of deals, 1 deal is a list in format {SKU: [number required, price]}
+DEALS['A'] = [3, 130]
 DEALS.append(['B', 2, 45])
 
 
@@ -9,13 +9,16 @@ DEALS.append(['B', 2, 45])
 # skus = unicode string
 def checkout(skus):
 
-	sku_deals = '' # will be a list of all SKUs in a deal
+	sku_deals = [] # will be a list of all SKUs in a deal
 	for deal in DEALS:
-		sku_deals = sku_deals + deal[0]
+		sku_deals.append(deal[0])
 
 	total_cost = 0
 	for sku in PRICES:
-		if SKU in sku_deals:
+		n = skus.count(sku)
+		if sku in sku_deals:
+			pass
+		else:
 
 
 	return total_cost
