bc9b482f9071119291d5a48375666233f8a75286
sbx_user1060 <sbx_user1060@ip-10-128-242-216.eu-west-2.compute.internal>
2019-03-24 16:07:42 +0000

    Sun Mar 24 16:07:42 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,16 +1,20 @@
+# data
+PRICES = {'A':50, 'B':30, 'C':20, 'D':15}
+DEALS = [] # list of deals, 1 deal is a list in format [SKU, number required, price, number have]
+DEALS.append(['A', 3, 130])
+DEALS.append(['B', 2, 45])
+
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
 
-	# data
-	prices = {'A':50, 'B':30, 'C':20, 'D':15}
-	deals = [] # list of deals, 1 deal is a list in format [SKU, number required, price]
-	deals.append(['A', 3, 130, 0])
-	deals.append(['B', 2, 45, 0])
+	sku_deals = [] 
+	for deal in DEALS:
+		sku_deals.append(deal[0])
 
 	# main loop
 	total_cost = 0
-	for sku in skus: # SKU is 1 character
-		if sku in prices: # hopefully will avoid delimiter which I do not know
+
 
 	return total_cost
